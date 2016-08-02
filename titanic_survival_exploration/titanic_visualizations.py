e1e83fcf7c41a1f8f58f62a500ec52b78d65eccd
Chandrasekar Sureshkumar <chandrusuresh@gmail.com>
2016-08-02 16:34:40 -0400

    Indepth analysis of titanic visualizations.

--- titanic_survival_exploration/titanic_visualizations.py
+++ titanic_survival_exploration/titanic_visualizations.py
@@ -142,4 +142,373 @@ def survival_stats(data, outcomes, key, filters = []):
         nan_outcomes = all_data[pd.isnull(all_data[key])]['Survived']
         print "Passengers with missing '{}' values: {} ({} survived, {} did not survive)".format( \
               key, len(nan_outcomes), sum(nan_outcomes == 1), sum(nan_outcomes == 0))
+		  
+def find_titles(names,survived):
+	titles = []
+	madeIt = []
+	total = []
+	count = -1
+	for i in range(len(names)):
+		count = count + 1
+		x = names[i].split(',')
+		tmp = x[1].split('.')
+		
+		if not tmp[0] in titles:
+			titles.insert(len(titles),tmp[0])
+			total.insert(len(total),1)
+			if survived[count] == 1:
+				madeIt.insert(len(madeIt),1)
+			else:
+				madeIt.insert(len(madeIt),0)
+		else:
+			ind = titles.index(tmp[0])
+			total[ind] = total[ind] + 1
+			if survived[count] == 1:
+				madeIt[ind] = madeIt[ind] + 1
+				
+	return titles, madeIt, total
 
+def filter_lastName(names, survived):
+	lastName = []
+	madeIt = []
+	total = []
+	count = -1
+	for i in range(len(names)):
+		count = count + 1
+		x = names[i].split(',')
+		
+		if not x[0] in lastName:
+			lastName.insert(len(lastName),x[0])
+			total.insert(len(total),1)
+			if survived[count] == 1:
+				madeIt.insert(len(madeIt),1)
+			else:
+				madeIt.insert(len(madeIt),0)
+		else:
+			ind = lastName.index(x[0])
+			total[ind] = total[ind] + 1
+			if survived[count] == 1:
+				madeIt[ind] = madeIt[ind] + 1
+				
+	return lastName, madeIt, total
+	
+def filter_fare(fares, survived, min_val, max_val):
+	min_fare = min_val
+	max_fare = max_val
+	bin_size = 0.2
+	madeIt = [0 for i in range(int(1/bin_size))]
+	total = [0 for i in range(int(1/bin_size))]
+	count = -1
+
+	for i in range(len(fares)):
+		if np.isnan(fares[i]):
+			continue
+		else:
+			bin_num = int(np.floor(fares[i]/(max_fare - min_fare)/bin_size))-1
+			if survived[i] == 1:
+				madeIt[bin_num] = madeIt[bin_num] + 1
+			total[bin_num] = total[bin_num] + 1
+						
+	return madeIt, total	
+	
+def filter_SibSp(SibSp, survived):
+	max_SibSp = int(np.max(SibSp))
+	bin_size = 1
+	madeIt = [0 for i in range(max_SibSp+1)]
+	total = [0 for i in range(max_SibSp+1)]
+
+	count = -1
+
+	for i in range(len(SibSp)):
+		if survived[i] == 1:
+			madeIt[SibSp[i]] = madeIt[SibSp[i]] + 1
+			
+		total[SibSp[i]] = total[SibSp[i]] + 1
+						
+	return madeIt, total	
+
+def filter_SexSibSp(SibSp, sex, survived, filter):
+	max_SibSp = int(np.max(SibSp))
+	bin_size = 1
+	madeIt = [0 for i in range(max_SibSp+1)]
+	total = [0 for i in range(max_SibSp+1)]
+
+	count = -1
+
+	for i in range(len(SibSp)):
+		if survived[i] == 1 and sex[i] == filter:
+			madeIt[SibSp[i]] = madeIt[SibSp[i]] + 1
+		if sex[i] == filter:
+			total[SibSp[i]] = total[SibSp[i]] + 1
+						
+	return madeIt, total
+	
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+
+# Print the first few entries of the RMS Titanic data
+print full_data.head()
+
+
+# From a sample of the RMS Titanic data, we can see the various features present for each passenger on the ship:
+# - **Survived**: Outcome of survival (0 = No; 1 = Yes)
+# - **Pclass**: Socio-economic class (1 = Upper class; 2 = Middle class; 3 = Lower class)
+# - **Name**: Name of passenger
+# - **Sex**: Sex of the passenger
+# - **Age**: Age of the passenger (Some entries contain `NaN`)
+# - **SibSp**: Number of siblings and spouses of the passenger aboard
+# - **Parch**: Number of parents and children of the passenger aboard
+# - **Ticket**: Ticket number of the passenger
+# - **Fare**: Fare paid by the passenger
+# - **Cabin** Cabin number of the passenger (Some entries contain `NaN`)
+# - **Embarked**: Port of embarkation of the passenger (C = Cherbourg; Q = Queenstown; S = Southampton)
+# 
+# Since we're interested in the outcome of survival for each passenger or crew member, we can remove the **Survived** feature from this dataset and store it as its own separate variable `outcomes`. We will use these outcomes as our prediction targets.  
+# Run the code cell below to remove **Survived** as a feature of the dataset and store it in `outcomes`.
+
+# Store the 'Survived' feature in a new variable and remove it from the dataset
+outcomes = full_data['Survived']
+data = full_data.drop('Survived', axis = 1)
+
+# Show the new dataset with 'Survived' removed
+print data.head()
+
+names = data["Name"]
+
+##############################################################
+############### Find patterns in title of name ###############
+##############################################################
+print "By Title"
+titles, madeIt, total = find_titles(full_data['Name'],full_data['Survived'])
+#print titles
+
+for i in range(len(titles)):
+	print titles[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " survived!!!"
+#	if madeIt[i]/(total[i]+0.0) > 0.8:
+#		print titles[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " survived!!!"
+	
+# Not much improvement can be made by filtering titles
+print
+print
+
+###############################################################################
+################# Find patterns by last name for the survived #################
+###############################################################################
+print "Filter of Survivors By Last Name"
+
+lastName, madeIt, total = filter_lastName(full_data['Name'],full_data['Survived'])
+#print titles
+
+for i in range(len(lastName)):
+	#print lastName[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " survived!!!"
+	if madeIt[i]/(total[i]+0.0) >= 0.70 and total[i] > 1:
+		print lastName[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " survived!!!"
+	
+print
+print
+	
+###############################################################################
+################# Find patterns by last name for the perished #################
+###############################################################################
+print "Filter of Non-Survivors By Last Name"
+
+lastName, madeIt, total = filter_lastName(full_data['Name'],full_data['Survived']+1)
+#print titles
+
+for i in range(len(lastName)):
+	#print lastName[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " did not survive!!!"
+	if madeIt[i]/(total[i]+0.0) > 0.70 and total[i] > 1:
+		print lastName[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " did not survive!!!"
+		
+## Noticed that names ending in son, sen perished.
+print
+print
+
+# find out the % of son, sen that perished.
+print "% of son, sen that perished"
+
+survived = full_data["Survived"]
+lastName = []
+madeIt = []
+total = []
+count = -1
+for i in range(len(names)):
+	count = count + 1
+	x = names[i].split(',')
+	
+	last3 = ""
+	if "sen" == x[0][-3:]:
+		last3 = "sen"
+	elif "son" == x[0][-3:]:
+		last3 = "son"
+	else:
+		continue
+	
+	if last3 == "":
+		continue
+	
+	if (not x[0][-3:] in lastName) or (lastName == []):
+		lastName.insert(len(lastName),x[0][-3:])
+		total.insert(len(total),1)
+		if survived[count] == 0:
+			madeIt.insert(len(madeIt),1)
+		else:
+			madeIt.insert(len(madeIt),0)
+	else:
+		ind = lastName.index(x[0][-3:])
+		total[ind] = total[ind] + 1
+		if survived[count] == 0:
+			madeIt[ind] = madeIt[ind] + 1
+				
+for i in range(len(lastName)):
+	#if madeIt[i]/(total[i]+0.0) > 0.80 and total[i] > 1:
+	print lastName[i] + " : " + str(madeIt[i]) + " out of " + str(total[i]) + " did not survive!!!"
+
+# 47 out of 65 whose name ends in son did not survive
+# 12 out of 14 whose name ends in sen did not survive
+	
+print
+print
+
+##############################################################
+################# Find patterns by fare #################
+##############################################################
+print "Fares"
+madeIt, total = filter_fare(full_data['Fare'],full_data['Survived'], np.min(full_data['Fare']), np.max(full_data['Fare']))
+
+print madeIt
+print total
+
+# 36 out of 50 whose ticket fare was less than 40% bracket survived.
+print
+print
+
+##########################################################
+################# Find patterns by SibSp #################
+##########################################################
+print "SibSp"
+madeIt, total = filter_SibSp(full_data['SibSp'],full_data['Survived'])
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+#################################################################
+################# Find patterns by SibSp & Male #################
+#################################################################
+print "SibSp & Male"
+madeIt, total = filter_SexSibSp(full_data['SibSp'],full_data['Sex'],full_data['Survived'],"male")
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+###################################################################
+################# Find patterns by SibSp & Female #################
+###################################################################
+print "SibSp & Female"
+madeIt, total = filter_SexSibSp(full_data['SibSp'],full_data['Sex'],full_data['Survived'],"female")
+
+print madeIt
+print total
+
+# 77% of females that have less than or equal to 2 siblings survived
+print
+print
+
+########################################################
+################# Find patterns by age #################
+########################################################
+print "Age"
+madeIt, total = filter_fare(full_data['Age'],full_data['Survived'],0,100)
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+########################################################
+################# Find patterns by Parch #################
+########################################################
+print "Parch"
+madeIt, total = filter_SibSp(full_data['Parch'],full_data['Survived'])
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+#####################################################################
+################# Find patterns by Parch and Female #################
+#####################################################################
+print "Parch & Female"
+madeIt, total = filter_SexSibSp(full_data['Parch'],full_data['Sex'],full_data['Survived'],"female")
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+#############################################################################
+################# Find patterns by Parch and Female and Age #################
+#############################################################################
+print "Parch & Female & 20<Age<60"
+ind1 = np.where(full_data["Age"] > 30)[0]
+ind2 = np.where(full_data["Age"] < 50)[0]
+#ind3 = np.where(full_data["SibSp"] > 0)[0]
+ind4 = np.where(full_data["Parch"] > 0)[0]
+ind5 = np.where(full_data["Sex"] == "female")[0]
+ind = list(set(ind1).intersection(ind2))
+#ind = list(set(ind).intersection(ind3))
+ind = list(set(ind).intersection(ind4))
+ind = list(set(ind).intersection(ind5))
+madeIt = 0
+total = 0
+for i in ind:
+	if full_data['Survived'][i] == 1:
+		madeIt = madeIt + 1
+	total = total + 1
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
+
+#############################################################
+################# Find patterns by Embarked #################
+#############################################################
+print "Embarked"
+embarked = list(set(full_data["Embarked"]))
+print embarked
+
+madeIt = [0 for i in embarked]
+total = [0 for i in embarked]
+
+for i in range(len(full_data["Embarked"])):
+	ind = embarked.index(full_data["Embarked"][i])
+	
+	if full_data["Survived"][i] == 1:
+		madeIt[ind] = madeIt[ind] + 1
+	total[ind] = total[ind] + 1
+
+print madeIt
+print total
+
+# Nothing significant
+print
+print
